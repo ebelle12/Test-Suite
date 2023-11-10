a51b42c8ccf58a8c60b1e8adb1e917dc6fe57109
Eva Belle <evabelle@Evas-Air.lan>
2023-11-10 04:31:20 -0600

    Final Push

---
+++ index.js
@@ -0,0 +1,40 @@
+// Create test for the following section to help with career simulation
+
+
+
+
+
+//Main page: 
+//1. list of all players
+// Each player on roster must have two buttons:
+//A details button for each player 
+// Must have: player's name, breed, and assigned team (or "unassigned" if they do not have a team)
+//A larger version of the picture
+//A button to go back to the main list
+//See the "Stretch Goals" section for more possible features
+//A "Remove" button that takes that player off the roster
+
+
+//Adding Player(s)
+//I need a form on the page that allows me to add a new player.
+//The form should have at least two inputs and a button:
+//One input for the player name
+//One input for the player's breed
+//A submit button to send the data and add the player to the roster
+//See the "Stretch Goals" section for more possible form features.
+//When entering a name and breed into the form and then hitting "submit", the new player should pop up in the roster without a page refr
+
+
+//Removing Player(s)
+//When player is removed they should disappear from roster without a page refresh.
+
+//Stretch Goals
+
+//Single Player View:
+//Show all of the teammates (all players assigned to the same team) as the current player.
+//dd a dropdown so that users can change the team assignment for the current player.
+//The team assignment should change in the single player view and in the roster immediately, without a page refresh.
+//Adding Players:
+//Add an input to the form which lets us provide an image URL when adding a player.
+//The linked image should show up as the player's portrait.
+
